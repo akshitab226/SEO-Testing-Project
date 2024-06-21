a3a81495f98d1b60aeaf0263473876d604262dfd
Akshita Badkundri <akshitab@terpmail.umd.edu>
2024-06-21 17:52:03 +0000

    changes to calc

--- calc.py
+++ calc.py
@@ -16,11 +16,11 @@ class Calc:
     def div(self, a, b):
         """Divide two numbers"""
         return a / b
-    
+
     def pow(self, a, b):
         """Raise a to the power of b"""
         return a ** b
-    
+
     def sqrt(self, a):
         """Return the square root of a"""
         return a ** 0.5
